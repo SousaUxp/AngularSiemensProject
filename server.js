a6cd04c5f1e348cb2c4147d331c6b50ab305f768
SousaUxp <jsousa.uxp@gmail.com>
2022-04-13 23:44:29 +0100

    updates to deploy to herokus

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/siemensproject'));
+app.use(express.static(__dirname + '/dist/siemens-Project'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/siemensproject/index.html'));
+        '/dist/siemens-Project/index.html'));
 });
 app.listen(process.env.PORT || 8080);
