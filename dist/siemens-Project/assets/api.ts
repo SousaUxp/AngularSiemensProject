afac5fd19f9705a68a0eb978af44d5036404e78f
SousaUxp <jsousa.uxp@gmail.com>
2022-04-13 23:18:23 +0100

    updates to deploy to herokus

---
+++ dist/siemens-Project/assets/api.ts
@@ -0,0 +1,1 @@
+export const apiLink = 'https://sousauxp.github.io/siemens.github.io/data/data.json'
---
+++ dist/siemens-Project/assets/interfaces.ts
@@ -0,0 +1,7 @@
+export interface projects {
+    name?: string; 
+    startDate?: string; 
+    endDate?: string; 
+    status?: string;
+    id?:string
+}
